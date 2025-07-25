50f7a13527b9c0b5b808dba29a1ae2b33e8f330b
Sora Takeshima <s1okkroyck.4@gmail.com>
2025-07-25 11:19:59 +0900

    アイテムページで画像を表示、編集ページ・削除ページへのリンクを作成

--- app/items/readsingle/[id]/page.js
+++ app/items/readsingle/[id]/page.js
@@ -1,6 +1,6 @@
 import { createClient } from "../../../utils/supabase/client"
-// import Image from "next/image"
-// import Link from "next/link"
+import Image from "next/image"
+import Link from "next/link"
 
 export async function generateMetadata(context){
     const params = await context.params
@@ -21,27 +21,25 @@ const getSingleItem = async(id) => {
         return data
 }
 
-//TODO:画像を表示させる、編集ページ・削除ページへのリンクを作る
-
 const ReadSingleItem = async(context) => {
     const params = await context.params
     const singleItemArray = await getSingleItem(params.id)
     const singleItem = singleItemArray[0]
     return (
         <div className="grid-container-si">
-            {/* <div>
+            <div>
                 <Image src={singleItem.image} width={750} height={500} alt="item-image" priority/>
-            </div> */}
+            </div>
             <div>
                 <h1>{singleItem.title}</h1>
                 <h2>¥{singleItem.price}</h2>
                 <hr/>
                 <p>{singleItem.description}</p>
             </div>
-            {/* <div>
-                <Link href={`/item/update/${singleItem._id}`}>アイテム編集</Link>
-                <Link href={`/item/delete/${singleItem._id}`}>アイテム削除</Link>
-            </div> */}
+            <div>
+                <Link href={`/items/update/${singleItem.id}`}>アイテム編集</Link>
+                <Link href={`/items/delete/${singleItem.id}`}>アイテム削除</Link>
+            </div>
         </div>
     )
 }
