72d093caefc1c8637f328be6afdedc226972ff53
Sora Takeshima <s1okkroyck.4@gmail.com>
2025-07-30 06:27:40 +0900

    createClientの引数を削除

--- app/items/readsingle/[id]/page.js
+++ app/items/readsingle/[id]/page.js
@@ -13,7 +13,7 @@ export async function generateMetadata(context){
 }
 
 const getSingleItem = async(id) => {
-        const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+        const supabase = createClient()
         const { data } = await supabase
             .from("all_items")
             .select()
